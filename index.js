16605708b99ab15e4ca69ae6fbfe86dcc729c394
Nat Epstein <nathaniel.epstein@gmail.com>
2023-01-05 22:30:08 -0500

    added preliminary logic: there is a control flow so that the user can ask a question in the browser through the prompt method and then get an answer back in the console. DOM manipulation to follow

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-console.log("I'm working whether you like it or not!");
+let question = '';
+// question = prompt("What is your question?");
+// console.log(question);
+let eightBall = '';
+let randomNumber = Math.floor(Math.random() * 8);
+console.log(randomNumber);
+
+switch (randomNumber){
+    case 0: 
+        eightBall = "It is certain"
+        break;
+    case 1: 
+        eightBall = "It is decidedly so"
+        break;
+    case 2:
+        eightBall = "Reply hazy, try again"
+        break;
+    case 3:
+        eightBall = "Cannot predict now"
+        break;
+    case 4: 
+        eightBall = "Hell no"
+        break;
+    case 5:
+        eightBall = "Maybe"
+        break;
+    case 6: 
+        eightBall = "Outlook not so good"
+        break;
+    case 7:
+        eightBall = "It doesn't seem so"
+        break;   
+}
+
+console.log(`The Magic 8 Ball says ${eightBall}`);
