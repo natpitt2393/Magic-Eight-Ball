55b7f397be19900528d140ce1d487a9743b255c8
Nat Epstein <nathaniel.epstein@gmail.com>
2023-01-09 15:32:43 -0500

    magic eight ball answer now displays on screen

--- index.js
+++ index.js
@@ -5,31 +5,50 @@ let eightBall = '';
 let randomNumber = Math.floor(Math.random() * 8);
 console.log(randomNumber);
 
-switch (randomNumber){
-    case 0: 
-        eightBall = "It is certain"
-        break;
-    case 1: 
-        eightBall = "It is decidedly so"
-        break;
-    case 2:
-        eightBall = "Reply hazy, try again"
-        break;
-    case 3:
-        eightBall = "Cannot predict now"
-        break;
-    case 4: 
-        eightBall = "Hell no"
-        break;
-    case 5:
-        eightBall = "Maybe"
-        break;
-    case 6: 
-        eightBall = "Outlook not so good"
-        break;
-    case 7:
-        eightBall = "It doesn't seem so"
-        break;   
+//DOM variables
+let questionButtonEl = document.querySelector('.questionButton')
+let answerEl = document.querySelector('.answer');
+
+
+function displayEightBallAnswer() {
+
+    switch (randomNumber) {
+        case 0:
+            eightBall = "It is certain"
+            break;
+        case 1:
+            eightBall = "It is decidedly so"
+            break;
+        case 2:
+            eightBall = "Reply hazy, try again"
+            break;
+        case 3:
+            eightBall = "Cannot predict now"
+            break;
+        case 4:
+            eightBall = "Hell no"
+            break;
+        case 5:
+            eightBall = "Maybe"
+            break;
+        case 6:
+            eightBall = "Outlook not so good"
+            break;
+        case 7:
+            eightBall = "It doesn't seem so"
+            break;
+
+
+    }
+    console.log(`The Magic 8 Ball says ${eightBall}`);
+   let eightBallAnswer = answerEl;
+   eightBallAnswer.textContent = eightBall;
+
 }
 
-console.log(`The Magic 8 Ball says ${eightBall}`);
+
+
+
+questionButtonEl.addEventListener('click', function () {
+    displayEightBallAnswer();
+})
